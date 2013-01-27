fa9076c5b7459da2350e493b5c60788404445d20
John Pancoast <shideon@gmail.com>
2013-01-27 09:52:10 -0800

    added Model placeholders

---
+++ classes/Deta/Core/Model.php
@@ -0,0 +1,13 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * Data model.
+ *
+ * @package Deta
+ * @author John Pancoast <shideon@gmail.com>
+ * @copyright 2012-2013 John Pancoast
+ * @license http://opensource.org/licenses/MIT MIT
+ * @abstract
+ */
+class Model {
+}
---
+++ classes/Deta/Model.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+
+class Deta_Model extends Deta_Core_Model {}
