93bb3856304a5cab346e62b82bfbdaefa5126365
John Pancoast <shideon@gmail.com>
2013-01-30 10:57:01 -0800

    textarea allows passing max length for pager

--- classes/Deta/Core/Model/Field/TextArea.php
+++ classes/Deta/Core/Model/Field/TextArea.php
@@ -3,14 +3,16 @@
 class Deta_Core_Model_Field_TextArea extends Deta_Model_Field {
 	public function add_form_field(Kohana_ORM $orm, Deta_Core_Form $form)
 	{
-		$field = Deta_Form_Field::factory('Textarea', $this->name, $this->label, $this->placeholder);
+		$field = Deta_Form_Field::factory('textarea', $this->name, $this->label, $this->placeholder);
 		$field->value($orm->{$this->name});
 		$form->field($field);
 	}
 
 	public function render_pager_field(Kohana_ORM $orm)
 	{
-		return $orm->{$this->name};
+		$max = isset($this->options['max_length']) ? $this->options['max_length'] : 100;
+		$value = $orm->{$this->name};
+		return strlen($value) >= $max ? substr($value, 0, $max).'...' : $value;
 	}
 
 	public function set_value(Kohana_ORM $orm, $value)
