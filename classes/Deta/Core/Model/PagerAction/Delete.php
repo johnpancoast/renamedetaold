1cf464dccbff733068b3ad0d6e3ae6fe1613a5eb
John Pancoast <shideon@gmail.com>
2013-01-29 17:25:43 -0800

    delete field has confirm dialog

--- classes/Deta/Core/Model/PagerAction/Delete.php
+++ classes/Deta/Core/Model/PagerAction/Delete.php
@@ -7,7 +7,7 @@ class Deta_Core_Model_PagerAction_Delete extends Deta_Core_Model_PagerAction {
 		return '<span class="pager_delete_link">'
 		. '<form method="POST" action="'.$this->link.'"">'
 		. '<input type="hidden" name="id" value="'.$values['id'].'" />'
-		. '<button type="submit" class="'.$class.'">Delete</button>'
+		. '<button type="submit" class="'.$class.'" onclick="if(!confirm(\'Are you sure you want to delete this entry?\')) { return false; }">Delete</button>'
 		. '</form>'
 		. '</span>';
 	}
